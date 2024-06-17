7ca7ef4ca244c2223d4e50c9e51ec587824834d3
Bartek <b.karpinski.kontakt@gmail.com>
2024-06-17 21:17:51 +0200

    abc

--- components/skills.tsx
+++ components/skills.tsx
@@ -21,7 +21,7 @@ export default function Skills () {
   const { ref } = useSectionInWiev('Umiejętności')
   return (
         <section ref={ref} id="Umiejętności" className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
-        <SectionHeading>Moje umiejętności</SectionHeading>
+        <SectionHeading>Moje umiejętności </SectionHeading>
             <ul className=" flex flex-wrap justify-center gap-2 text-lg text-gray-800 ">
                 {skillsData.map((skill, index) => (
                     <motion.li className="bg-white border border-black/[0.1]  rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
